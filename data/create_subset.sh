3c959bb833cb3c5b66921efb7306f23e02bcdfd3
Norman Poh <NPoh@uk.imshealth.com>
2017-09-05 09:30:18 +0100

    created the subseting solution to handle IPF_cohort misalignment problem

--- data/create_subset.sh
+++ data/create_subset.sh
@@ -1,6 +1,19 @@
+# copy to my local computer
 for m in IPF_Cohort.csv/IPF_Cohort.csv Non_IPF_Asthma_COPD_Cohort.csv/Non_IPF_Asthma_COPD_Cohort.csv Representative_Sample_Asthma_COPD_Cohort.csv/Representative_Sample_Asthma_COPD_Cohort.csv Scoring_Sample_V2.csv/Scoring_Sample_V2.csv
 do
 	echo $m
 	fname=`basename $m .csv`
 	sed -n -e '1,1000p' $m > "norman_subset/${fname}.csv"
 done
+
+
+# run on server
+cd "F:\Hui\Project_2016\BI_IPF_2016\02_data"
+for m in IPF_Cohort.csv/IPF_Cohort.csv Non_IPF_Asthma_COPD_Cohort.csv/Non_IPF_Asthma_COPD_Cohort.csv Representative_Sample_Asthma_COPD_Cohort.csv/Representative_Sample_Asthma_COPD_Cohort.csv Scoring_Sample_V2.csv/Scoring_Sample_V2.csv
+do
+	echo $m
+	fname=`basename $m .csv`
+	cp $m "/f/Norman/BI_IPF2017/data/raw/${fname}.csv"
+done
+
+
